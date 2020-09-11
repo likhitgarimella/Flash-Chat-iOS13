b73740af7211d063251924ff55cc56667c8d1c32
Likhit <likhitgmella@gmail.com>
2020-09-11 22:50:14 +0530

    login users

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     
@@ -23,8 +24,17 @@ class LoginViewController: UIViewController {
     
     @IBAction func loginPressed(_ sender: UIButton) {
         
-        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    // Navigate to chat
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+            }
+        }
         
     }
     
-}   // #31
+}   // #41
