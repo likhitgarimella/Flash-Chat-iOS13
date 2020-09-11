5488228caaf8eb1503e7f00a05d0983bbf4aeeeb
Likhit <likhitgmella@gmail.com>
2020-09-12 01:55:14 +0530

    message cell file & xib added

---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,27 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Likhit Garimella on 12/09/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        
+        
+    }
+    
+}   // #28
