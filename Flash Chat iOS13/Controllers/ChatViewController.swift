3809035a5b6775ba1284c20a2b2fa3ead911f9c6
Likhit <likhitgmella@gmail.com>
2020-09-12 02:20:24 +0530

    data popln in xib cell trial

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -28,6 +28,8 @@ class ChatViewController: UIViewController {
         title = K.appName
         navigationItem.hidesBackButton = true
         
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -56,10 +58,10 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row].body
-        cell.textLabel?.textColor = UIColor.darkGray
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label?.text = messages[indexPath.row].body
+        cell.label?.textColor = UIColor.darkGray
         return cell
     }
     
-}   // #66
+}   // #68
