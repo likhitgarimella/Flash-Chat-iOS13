bba4a5e591597b0876683347fcf446c694260b7f
Likhit <likhitgmella@gmail.com>
2020-09-11 21:51:05 +0530

    firebase app configure

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
@@ -30,4 +34,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
-}   // #34
+}   // #38
