484321fc130a7a7a63803add37ca3192af088802
Likhit <likhitgmella@gmail.com>
2020-09-12 00:34:16 +0530

    model Message created

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Likhit Garimella on 12/09/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+}   // #14
