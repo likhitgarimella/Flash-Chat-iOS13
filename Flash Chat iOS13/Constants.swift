ebf3d541d3fae40beaa1e203d41ec3fa91da9618
Likhit <likhitgmella@gmail.com>
2020-09-11 23:36:56 +0530

    constants file

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Likhit Garimella on 11/09/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+struct Constants {
+    
+}   // #12
